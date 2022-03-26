35f46ac9bc61ac2a7ebfa2e418490f2d553676ef
KolesnichenkoAG <kag.1981@mail.ru>
2022-03-26 23:06:29 +0600

    начало

---
+++ src/main/java/lesson6/Animal.java
@@ -0,0 +1,19 @@
+package lesson6;
+
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    public void animalInfo() {
+        System.out.println("Животное: " + name);
+    }
+    public void run() {
+        System.out.println("Животное бежит");
+    }
+    public void swim() {
+        System.out.println("Животное плывет");
+    }
+
+}
